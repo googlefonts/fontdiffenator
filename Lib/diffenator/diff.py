98cb52eba1ae8255f16934bc4ae5bc7a0f6ada9f
Marc Foley <m.foley.88@gmail.com>
2018-04-22 22:17:19 +0100

    General purpose _subtract function added

--- Lib/diffenator/diff.py
+++ Lib/diffenator/diff.py
@@ -49,7 +49,7 @@ anchors_a_h = {r['glyph']: r for r in table}
 
 We could even make a key by using coords
 
-anchors_a_h = {(r["base_x"], r["base_y"]: r for r in table)}
+anchors_a_h = {(r["base_x"], r["base_y"]): r for r in table)}
 
 {(20, 40): {"glyph" "x", "base_x": 20, "base_y": 40}}
 
@@ -142,41 +142,32 @@ def diff_nametable(font_a, font_b):
     nametable_a = dump_nametable(font_a)
     nametable_b = dump_nametable(font_b)
 
-    missing = _subtract_names(nametable_a, nametable_b)
-    new = _subtract_names(nametable_b, nametable_a)
-    modified = _modified_names(nametable_a, nametable_b)
+    names_a_h = {i['id']: i for i in nametable_a}
+    names_b_h = {i['id']: i for i in nametable_b}
+
+    missing = _subtract_items(names_a_h, names_b_h)
+    new = _subtract_items(names_b_h, names_a_h)
+    modified = _modified_names(names_a_h, names_b_h)
 
     Names = namedtuple('Names', ['new', 'missing', 'modified'])
-    return Names(new, missing, modified)
+    return Names(
+        sorted(new, key=lambda k: k['id']),
+        sorted(missing, key=lambda k: k['id']),
+        sorted(modified, key=lambda k: k['id']),
+    )
 
 
-def _subtract_names(nametable_a, nametable_b):
+def _modified_names(names_a, names_b):
 
-    names_a_h = {i['id']: i for i in nametable_a}
-    names_b_h = {i['id']: i for i in nametable_b}
-
-    missing = set(names_a_h.keys()) - set(names_b_h.keys())
-
-    table = []
-    for k in missing:
-        table.append(names_a_h[k])
-    return table
-
-
-def _modified_names(nametable_a, nametable_b):
-
-    names_a_h = {i['id']: i for i in nametable_a}
-    names_b_h = {i['id']: i for i in nametable_b}
-
-    shared = set(names_a_h.keys()) & set(names_b_h.keys())
+    shared = set(names_a.keys()) & set(names_b.keys())
 
     table = []
     for k in shared:
-        if names_a_h[k]['string'] != names_b_h[k]['string']:
+        if names_a[k]['string'] != names_b[k]['string']:
             row = {
-                'id': names_a_h[k]['id'],
-                'string_a': names_a_h[k]['string'],
-                'string_b': names_b_h[k]['string']
+                'id': names_a[k]['id'],
+                'string_a': names_a[k]['string'],
+                'string_b': names_b[k]['string']
             }
             table.append(row)
     return table
@@ -184,16 +175,22 @@ def _modified_names(nametable_a, nametable_b):
 
 @timer
 def diff_glyphs(font_a, font_b):
-    # TODO (M FOLEY) work shape diff in here
     glyphs_a = dump_glyphs(font_a)
     glyphs_b = dump_glyphs(font_b)
 
-    missing = _subtract_glyphs(glyphs_a, glyphs_b)
-    new = _subtract_glyphs(glyphs_b, glyphs_a)
-    modified = _modified_glyphs(glyphs_a, glyphs_b)
+    glyphs_a_h = {r['glyph'].kkey: r for r in glyphs_a}
+    glyphs_b_h = {r['glyph'].kkey: r for r in glyphs_b}
+
+    missing = _subtract_items(glyphs_a_h, glyphs_b_h)
+    new = _subtract_items(glyphs_b_h, glyphs_a_h)
+    modified = _modified_glyphs(glyphs_a_h, glyphs_b_h)
 
     Glyphs = namedtuple('Glyphs', ['new', 'missing', 'modified'])
-    return Glyphs(new, missing, modified)
+    return Glyphs(
+        sorted(new, key=lambda k: k['glyph'].name),
+        sorted(missing, key=lambda k: k['glyph'].name),
+        sorted(modified, key=lambda k: k['diff'], reverse=True)
+    )
 
 
 @timer
@@ -202,43 +199,38 @@ def diff_kerning(font_a, font_b):
     kern_a = dump_kerning(font_a)
     kern_b = dump_kerning(font_b)
 
-    missing = _subtract_kerns(kern_a, kern_b)
-    new = _subtract_kerns(kern_b, kern_a)
-    modified = _modified_kerns(kern_a, kern_b)
-
-    Kern = namedtuple('KernDiff', ['new', 'missing', 'modified'])
-    return Kern(new, missing, modified)
-
-
-def _subtract_kerns(kern_a, kern_b):
-
     kern_a_h = {i['left'].kkey + i['right'].kkey: i for i in kern_a}
     kern_b_h = {i['left'].kkey + i['right'].kkey: i for i in kern_b}
 
-    missing_kerns = set(kern_a_h.keys()) - set(kern_b_h.keys())
+    missing = _subtract_items(kern_a_h, kern_b_h)
+    new = _subtract_items(kern_b_h, kern_a_h)
+    modified = _modified_kerns(kern_a_h, kern_b_h)
 
-    table = []
-    for k in missing_kerns:
-        table.append(kern_a_h[k])
+    Kern = namedtuple('KernDiff', ['new', 'missing', 'modified'])
+    return Kern(
+        sorted(new, key=lambda k: k['left'].name),
+        sorted(missing, key=lambda k: k['left'].name),
+        sorted(modified, key=lambda k: abs(k['diff']), reverse=True)
+    )
 
-    return sorted(table, key=lambda k: abs(k['value']), reverse=True)
+
+def _subtract_items(items_a, items_b):
+    subtract = set(items_a.keys()) - set(items_b.keys())
+    return [items_a[i] for i in subtract]
 
 
 def _modified_kerns(kern_a, kern_b):
 
-    kern_a_h = {i['left'].kkey + i['right'].kkey: i for i in kern_a}
-    kern_b_h = {i['left'].kkey + i['right'].kkey: i for i in kern_b}
-
-    shared = set(kern_a_h.keys()) & set(kern_b_h.keys())
+    shared = set(kern_a.keys()) & set(kern_b.keys())
 
     table = []
     for k in shared:
-        if kern_a_h[k]['value'] != kern_b_h[k]['value']:
-            kern_diff = kern_a_h[k]
-            kern_diff['diff'] = kern_b_h[k]['value'] - kern_a_h[k]['value']
+        if kern_a[k]['value'] != kern_b[k]['value']:
+            kern_diff = kern_a[k]
+            kern_diff['diff'] = kern_b[k]['value'] - kern_a[k]['value']
             del kern_diff['value']
             table.append(kern_diff)
-    return sorted(table, key=lambda k: abs(k['diff']), reverse=True)
+    return table
 
 
 @timer
@@ -249,7 +241,9 @@ def diff_metrics(font_a, font_b):
     modified = _modified_metrics(metrics_a, metrics_b)
 
     Metrics = namedtuple('Metrics', ['modified'])
-    return Metrics(modified)
+    return Metrics(
+        sorted(modified, key=lambda k: k['diff_adv'], reverse=True)
+    )
 
 
 def _modified_metrics(metrics_a, metrics_b):
@@ -267,7 +261,7 @@ def _modified_metrics(metrics_a, metrics_b):
             metrics['diff_lsb'] = metrics_b_h[k]['lsb'] - metrics_b_h[k]['lsb']
             metrics['diff_rsb'] = metrics_b_h[k]['rsb'] - metrics_b_h[k]['rsb']
             table.append(metrics)
-    return sorted(table, key=lambda k: k['diff_adv'], reverse=True)
+    return table
 
 
 @timer
@@ -300,33 +294,18 @@ def _modified_attribs(attribs_a, attribs_b):
     return table
 
 
-def _subtract_glyphs(glyphs_a, glyphs_b):
-    glyphs_a_h = {r['glyph'].kkey: r for r in glyphs_a}
-    glyphs_b_h = {r['glyph'].kkey: r for r in glyphs_b}
-
-    missing = set(glyphs_a_h.keys()) - set(glyphs_b_h.keys())
-
-    table = []
-    for k in missing:
-        if glyphs_a_h[k]['glyph'].characters:
-            table.append(glyphs_a_h[k])
-    return sorted(table, key=lambda k: k['glyph'].name)
-
-
 def _modified_glyphs(glyphs_a, glyphs_b):
-    glyphs_a_h = {r['glyph'].kkey: r for r in glyphs_a}
-    glyphs_b_h = {r['glyph'].kkey: r for r in glyphs_b}
 
-    shared = set(glyphs_a_h.keys()) & set(glyphs_b_h.keys())
+    shared = set(glyphs_a.keys()) & set(glyphs_b.keys())
 
     table = []
     for k in shared:
-        if glyphs_a_h[k]['area'] != glyphs_b_h[k]['area']:
+        if glyphs_a[k]['area'] != glyphs_b[k]['area']:
             table.append({
-                "glyph": glyphs_a_h[k]['glyph'],
-                "diff":  glyphs_b_h[k]['area'] - glyphs_a_h[k]['area']
+                "glyph": glyphs_a[k]['glyph'],
+                "diff":  glyphs_b[k]['area'] - glyphs_a[k]['area']
             })
-    return sorted(table, key=lambda k: k['diff'], reverse=True)
+    return table
 
 
 @timer
@@ -345,46 +324,37 @@ def diff_marks(font_a, font_b):
     marks_a = _compress_to_single_mark(marks_a)
     marks_b = _match_marks_in_table(marks_b, marks_a)
 
-    missing = _subtract_marks(marks_a, marks_b)
-    new = _subtract_marks(marks_b, marks_a)
-    modified = _modified_marks(marks_a, marks_b)
-
-    Marks = namedtuple('Marks', ['new', 'missing', 'modified'])
-    return Marks(new, missing, modified)
-
-
-def _subtract_marks(marks_a, marks_b):
-
     marks_a_h = {i['base_glyph'].kkey+i['mark_glyph'].kkey: i for i in marks_a}
     marks_b_h = {i['base_glyph'].kkey+i['mark_glyph'].kkey: i for i in marks_b}
 
-    missing = set(marks_a_h.keys()) - set(marks_b_h.keys())
+    missing = _subtract_items(marks_a_h, marks_b_h)
+    new = _subtract_items(marks_b_h, marks_a_h)
+    modified = _modified_marks(marks_a_h, marks_b_h)
 
-    table = []
-    for k in missing:
-        table.append(marks_a_h[k])
-    return sorted(table, key=lambda k: k['base_glyph'].name)
+    Marks = namedtuple('Marks', ['new', 'missing', 'modified'])
+    return Marks(
+        sorted(new, key=lambda k: k['base_glyph'].name, reverse=True),
+        sorted(missing, key=lambda k: k['base_glyph'].name, reverse=True),
+        sorted(modified, key=lambda k: abs(k['diff_x']) + abs(k['diff_y']), reverse=True)
+    )
 
 
 def _modified_marks(marks_a, marks_b, ignore_thresh=8):
 
-    marks_a_h = {i['base_glyph'].kkey+i['mark_glyph'].kkey: i for i in marks_a}
-    marks_b_h = {i['base_glyph'].kkey+i['mark_glyph'].kkey: i for i in marks_b}
-
-    shared = set(marks_a_h.keys()) & set(marks_b_h.keys())
+    shared = set(marks_a.keys()) & set(marks_b.keys())
 
     table = []
     for k in shared:
-        offset_a_x = marks_a_h[k]['base_x'] - marks_a_h[k]['mark_x']
-        offset_a_y = marks_a_h[k]['base_y'] - marks_a_h[k]['mark_y']
-        offset_b_x = marks_b_h[k]['base_x'] - marks_b_h[k]['mark_x']
-        offset_b_y = marks_b_h[k]['base_y'] - marks_b_h[k]['mark_y']
+        offset_a_x = marks_a[k]['base_x'] - marks_a[k]['mark_x']
+        offset_a_y = marks_a[k]['base_y'] - marks_a[k]['mark_y']
+        offset_b_x = marks_b[k]['base_x'] - marks_b[k]['mark_x']
+        offset_b_y = marks_b[k]['base_y'] - marks_b[k]['mark_y']
 
         diff_x = offset_a_x != offset_b_x
         diff_y = offset_a_y != offset_b_y
 
         if diff_x or diff_y:
-            mark = marks_a_h[k]
+            mark = marks_a[k]
             mark['diff_x'] = offset_b_x - offset_a_x
             mark['diff_y'] = offset_b_y - offset_a_y
             if abs(mark['diff_x']) + abs(mark['diff_y']) < ignore_thresh:
@@ -392,9 +362,7 @@ def _modified_marks(marks_a, marks_b, ignore_thresh=8):
             for pos in ['base_x', 'base_y', 'mark_x', 'mark_y']:
                 mark.pop(pos)
             table.append(mark)
-
-    return sorted(table, key=lambda k: abs(k['diff_x']) + abs(k['diff_y']),
-                  reverse=True)
+    return table
 
 
 def _compress_to_single_mark(table):
