94f6bc77fa6ef14614541d37bc9bb8b741c00a5f
Marc Foley <m.foley.88@gmail.com>
2018-07-19 15:31:08 +0100

    Call diff_fonts with correct params

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -14,6 +14,7 @@ and run them through our main diff_fonts functions.
 This test is slow and should be run on challenging fonts.
 """
 from diffenator.diff import diff_fonts
+from diffenator.font import InputFont
 from itertools import permutations
 import collections
 from glob import glob
@@ -30,7 +31,9 @@ class TestFunctionality(unittest.TestCase):
 
     def test_diff(self):
         for font_a_path, font_b_path in self.font_path_combos:
-            diff = diff_fonts(font_a_path, font_b_path)
+            font_a = InputFont(font_a_path)
+            font_b = InputFont(font_b_path)
+            diff = diff_fonts(font_a, font_b)
             self.assertNotEqual(diff, collections.defaultdict(dict))
 
 
