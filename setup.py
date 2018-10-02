26c45506b588249c3a41ce86941bf2bcf3188b25
Marc Foley <m.foley.88@gmail.com>
2018-10-02 17:59:03 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils import log
 
 setup(
     name='fontdiffenator',
-    version='0.3.2',
+    version='0.4.2',
     author="Google Fonts Project Authors",
     description="Font regression tester for Google Fonts",
     url="https://github.com/googlefonts/diffenator",
