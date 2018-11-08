b9246261b361bdecf8f4b5aed885e039f7f11432
Marc Foley <m.foley.88@gmail.com>
2018-11-08 11:00:21 +0000

    Bump version number

--- Lib/diffenator/__init__.py
+++ Lib/diffenator/__init__.py
@@ -1,2 +1,2 @@
 from diffenator.diff import diff_fonts
-__version__ = "0.4.4"
+__version__ = "0.4.5"
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils import log
 
 setup(
     name='fontdiffenator',
-    version='0.4.4',
+    version='0.4.5',
     author="Google Fonts Project Authors",
     description="Font regression tester for Google Fonts",
     url="https://github.com/googlefonts/diffenator",
