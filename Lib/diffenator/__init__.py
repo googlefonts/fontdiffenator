986545450563f0c5b17789e542562cc239d7f237
Marc Foley <m.foley.88@gmail.com>
2019-01-02 18:12:28 +0000

    Bump version number

--- Lib/diffenator/__init__.py
+++ Lib/diffenator/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.1"
+__version__ = "0.6.1"
 import sys
 if sys.version_info[0] < 3 and sys.version_info[1] < 6:
     raise ImportError("Visualize module requires Python3.6+!")
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils import log
 
 setup(
     name='fontdiffenator',
-    version='0.5.1',
+    version='0.6.1',
     author="Google Fonts Project Authors",
     description="Font regression tester for Google Fonts",
     url="https://github.com/googlefonts/diffenator",
