1a1c5e615a78578e4195005099d94c0a279679a6
Marc Foley <m.foley.88@gmail.com>
2019-03-08 15:07:39 +0000

    Upgrade PIL dependency

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         "fonttools>=3.34.2",
         "fonttools>=3.28.0",
-	"Pillow==5.3.0",
+	"Pillow==5.4.1",
         "pycairo==1.18.0",
         "uharfbuzz==0.3.0",
         "freetype-py==2.0.0.post6",
