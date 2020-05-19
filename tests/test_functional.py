e75888d4143e78e2ea4180d55aec98fbd1908123
guidotheelen <guido@kabisa.nl>
2020-05-19 09:50:24 +0200

    Add CBDT tests

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -12,10 +12,7 @@ paths = ['path/to/font_a', 'path/to/font_b']
 This test is slow and should be run on challenging fonts.
 """
 from diffenator import CHOICES
-from diffenator.diff import DiffFonts
-from diffenator.font import DFont
 from itertools import permutations
-import collections
 import subprocess
 from glob import glob
 import os
@@ -29,9 +26,23 @@ class TestFunctionality(unittest.TestCase):
         self._path = os.path.dirname(__file__)
         font_paths = glob(os.path.join(self._path, 'data', '*.ttf'))
         self.font_path_combos = permutations(font_paths, r=2)
+        cbdt_font_paths = glob(os.path.join(self._path, 'data', 'cbdt_test', '*.ttf'))
+        self.cbdt_font_path_combos = permutations(cbdt_font_paths, r=2)
 
     def test_diff(self):
         for font_a_path, font_b_path in self.font_path_combos:
+            gif_dir = tempfile.mktemp()
+            subprocess.call([
+                "diffenator",
+                font_a_path,
+                font_b_path,
+                "-r", gif_dir])
+            gifs = [f for f in os.listdir(gif_dir) if f.endswith(".gif")]
+            self.assertNotEqual(gifs, [])
+            shutil.rmtree(gif_dir)
+
+    def test_cbdt_diff(self):
+        for font_a_path, font_b_path in self.cbdt_font_path_combos:
             gif_dir = tempfile.mktemp()
             subprocess.call([
                 "diffenator",
@@ -52,6 +63,16 @@ class TestFunctionality(unittest.TestCase):
         ])
 
         self.assertNotEqual(cmd, None)
+
+    def test_cbdt_dump(self):
+        font_path = os.path.join(self._path, 'data', 'cbdt_test', 'NotoColorEmoji-u11-u1F349.ttf')
+        for category in CHOICES:
+            cmd = subprocess.check_output([
+                "dumper",
+                font_path,
+                category,
+            ])
+            self.assertNotEqual(cmd, None)
 
     def test_dump(self):
         font_path = os.path.join(self._path, 'data', 'Play-Regular.ttf')
